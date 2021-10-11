747cf96eb6c444b55e2eec13ca5ca44d89fada4f
Amal R <amalr405@gmail.com>
2021-10-11 16:31:33 +0530

    Add files via upload

---
+++ Emszuulapi/src/test/java/com/ust/ems/zuulapi/EmszuulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.ems.zuulapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmszuulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
